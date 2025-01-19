f5403a42ac57c7191ecf0df6e0c812469cdd045f
tennylaylor <Lennyttaylor@gmail.com>
2025-01-19 13:27:55 -0500

    update /weekelyCalnedar, calendarrout, server.js

--- src/services/calendarService.js
+++ src/services/calendarService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/calendar";
+const API_URL = import.meta.env.VITE_BACKEND_URL + "/api/calendar";
 
 export const getWeeklyEvents = () => axios.get(API_URL);
 export const addWeeklyEvent = (day, event) =>
